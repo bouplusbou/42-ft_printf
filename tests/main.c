f6080d2eaa655f6304e17376b11605525a1d3375
Baptiste BOUCHER <bboucher@e1r2p6.42.fr>
2019-02-22 08:47:21 +0100

    clean main.c

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: bboucher <bboucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 14:00:10 by bclaudio          #+#    #+#             */
-/*   Updated: 2019/02/22 08:28:55 by bboucher         ###   ########.fr       */
+/*   Updated: 2019/02/22 08:47:06 by bboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,129 +14,5 @@
 
 int main()
 {
-	int		nb;
-	char	c;
-
-	nb = 0;
-	c = 'W';
-	printf("\n===|%5p$\n", &nb);
-	ft_printf(">>>|%5p$\n", &nb);
-	printf("\n===|%-15p$\n", &nb);
-	ft_printf(">>>|%-15p$\n", &nb);
-	printf("\n===|%-5p$\n", &nb);
-	ft_printf(">>>|%-5p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%-18p$\n", &nb);
-	ft_printf(">>>|%-18p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%5p$\n", &c);
-	ft_printf(">>>|%5p$\n", &c);
-	printf("\n===|%-15p$\n", &c);
-	ft_printf(">>>|%-15p$\n", &c);
-	printf("\n===|%-5p$\n", &c);
-	ft_printf(">>>|%-5p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%-18p$\n", &c);
-	ft_printf(">>>|%-18p$\n", &c);
-	printf("\n===|%5p$\n", &nb);
-	ft_printf(">>>|%5p$\n", &nb);
-	printf("\n===|%-15p$\n", &nb);
-	ft_printf(">>>|%-15p$\n", &nb);
-	printf("\n===|%-5p$\n", &nb);
-	ft_printf(">>>|%-5p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%-18p$\n", &nb);
-	ft_printf(">>>|%-18p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%5p$\n", &c);
-	ft_printf(">>>|%5p$\n", &c);
-	printf("\n===|%-15p$\n", &c);
-	ft_printf(">>>|%-15p$\n", &c);
-	printf("\n===|%-5p$\n", &c);
-	ft_printf(">>>|%-5p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%-18p$\n", &c);
-	ft_printf(">>>|%-18p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%5p$\n", &nb);
-	ft_printf(">>>|%5p$\n", &nb);
-	printf("\n===|%-15p$\n", &nb);
-	ft_printf(">>>|%-15p$\n", &nb);
-	printf("\n===|%-5p$\n", &nb);
-	ft_printf(">>>|%-5p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%-18p$\n", &nb);
-	ft_printf(">>>|%-18p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%5p$\n", &c);
-	ft_printf(">>>|%5p$\n", &c);
-	printf("\n===|%-15p$\n", &c);
-	ft_printf(">>>|%-15p$\n", &c);
-	printf("\n===|%-5p$\n", &c);
-	ft_printf(">>>|%-5p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%-18p$\n", &c);
-	ft_printf(">>>|%-18p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%5p$\n", &nb);
-	ft_printf(">>>|%5p$\n", &nb);
-	printf("\n===|%-15p$\n", &nb);
-	ft_printf(">>>|%-15p$\n", &nb);
-	printf("\n===|%-5p$\n", &nb);
-	ft_printf(">>>|%-5p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%-18p$\n", &nb);
-	ft_printf(">>>|%-18p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%5p$\n", &c);
-	ft_printf(">>>|%5p$\n", &c);
-	printf("\n===|%-15p$\n", &c);
-	ft_printf(">>>|%-15p$\n", &c);
-	printf("\n===|%-5p$\n", &c);
-	ft_printf(">>>|%-5p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%-18p$\n", &c);
-	ft_printf(">>>|%-18p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%5p$\n", &nb);
-	ft_printf(">>>|%5p$\n", &nb);
-	printf("\n===|%-15p$\n", &nb);
-	ft_printf(">>>|%-15p$\n", &nb);
-	printf("\n===|%-5p$\n", &nb);
-	ft_printf(">>>|%-5p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%-18p$\n", &nb);
-	ft_printf(">>>|%-18p$\n", &nb);
-	printf("\n===|%42p$\n", &nb);
-	ft_printf(">>>|%42p$\n", &nb);
-	printf("\n===|%5p$\n", &c);
-	ft_printf(">>>|%5p$\n", &c);
-	printf("\n===|%-15p$\n", &c);
-	ft_printf(">>>|%-15p$\n", &c);
-	printf("\n===|%-5p$\n", &c);
-	ft_printf(">>>|%-5p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-	printf("\n===|%-18p$\n", &c);
-	ft_printf(">>>|%-18p$\n", &c);
-	printf("\n===|%42p$\n", &c);
-	ft_printf(">>>|%42p$\n", &c);
-
 	return (0);
 }
