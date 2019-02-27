6c3d2b27ee24649d50612a7d978084428a819d19
Baptiste BOUCHER <bboucher@e1r2p6.42.fr>
2019-02-27 10:48:09 +0100

    clean main.c & delete coucou.txt

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: bboucher <bboucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 16:53:03 by bboucher          #+#    #+#             */
-/*   Updated: 2019/02/27 10:45:49 by bboucher         ###   ########.fr       */
+/*   Updated: 2019/02/27 10:48:07 by bboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,5 @@
 
 int main()
 {
-	// int	pf;
-
-	// pf = fopen("coucou.txt", "w");
-	
-	// if (!pf)
-	// 	printf("SORRY\n");
-	// else
-	// {
-	// 	printf("OK\n");
-	// 	// fprintf(pf, "%s", "WRITEOK");
-		ft_printf("{fd}*%*s\n", 2, "saucisse");
-	// }
-	// fprintf(stderr, "%s", "Stack overflow!\n");
 	return (0);
 }
